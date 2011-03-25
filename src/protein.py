cef71d8b0f6e2a56b57a39e55ae1ea90a530f635
Christian Fufezan <christian@fufezan.net>
2011-03-25 16:25:23 +0100

    added changes from Enrico Morelli.

--- src/protein.py
+++ src/protein.py
@@ -148,7 +148,7 @@ class Protein:
         (?P<beta>[0-9, ,\-,\.]{6})              # beta field
         ([ ]{10})                               # -Space-
         (?P<elementType>[A-Z,0-9, ]{2})         # Element Type
-        (?P<charge>[A-Z,0-9, ]{2})              # Charge
+        (?P<charge>[A-Z,0-9, ]*)                # Charge
         """
         '''
         NEW and not identified :
